e04e533bce54f410764f9e1119deba8763e78719
Mateusz Korycinski <mat.korycinski@gmail.com>
2017-01-29 18:24:31 +0100

    Added testing if module is run as a standalone script. Added doctest for NoConnection exception.

--- TaxonomyDB.py
+++ TaxonomyDB.py
@@ -30,6 +30,16 @@ class Node(object):
 class NoConnection(Exception):
     """Exception that is raised by each method that requires connection
     to the database
+
+    e.g.:
+    >>> raise NoConnection
+    Traceback (most recent call last):
+      File "/usr/lib/python2.7/doctest.py", line 1315, in __run
+        compileflags, 1) in test.globs
+      File "<doctest __main__.NoConnection[0]>", line 1, in <module>
+        raise NoConnection
+    NoConnection: 'No connection to the database! Exiting.'
+
     """
 
     def __str__(self):
@@ -159,3 +169,6 @@ class TaxDb(object):
 
         pass
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
